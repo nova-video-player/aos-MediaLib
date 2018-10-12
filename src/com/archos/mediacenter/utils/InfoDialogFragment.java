bf9897e70d76fa21049de3405c6f3891fe9b12a6
Marc de Courville <marc@courville.org>
2018-10-12 09:25:50 +0000

    Switch to support libraries

--- src/com/archos/mediacenter/utils/InfoDialogFragment.java
+++ src/com/archos/mediacenter/utils/InfoDialogFragment.java
@@ -15,7 +15,7 @@
 package com.archos.mediacenter.utils;
 
 import android.app.Dialog;
-import android.app.DialogFragment;
+import android.support.v4.app.DialogFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
