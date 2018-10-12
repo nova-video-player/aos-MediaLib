3a97a246db056807a2d96e4290abe88f6bd6a53b
Marc de Courville <marc@courville.org>
2018-10-12 07:31:57 +0000

    Change in ScraperPreferences PreferenceActivity into an AppCompatActivity and PreferenceFragmentCompat

--- src/com/archos/mediascraper/settings/ScraperPreferences.java
+++ src/com/archos/mediascraper/settings/ScraperPreferences.java
@@ -15,32 +15,18 @@
 package com.archos.mediascraper.settings;
 
 import com.archos.medialib.R;
-import com.archos.mediascraper.xml.BaseScraper2;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.CheckBoxPreference;
-import android.preference.EditTextPreference;
-import android.preference.ListPreference;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceManager;
-import android.preference.PreferenceScreen;
-import java.util.Arrays;
-import java.util.Enumeration;
-import java.util.Hashtable;
-import java.util.Locale;
-import java.util.List;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 
 /*
  * Preference screen generator.
  * Generate on-the-fly preference screen for a scraper, relying on its GetSettings
  * function.
  */
-public class ScraperPreferences extends PreferenceActivity {
 
-    private PreferenceManager mPm;
-    private BaseScraper2 mScraper;
-    private ScraperSettings mSettings;
-
+public class ScraperPreferences extends AppCompatActivity {
     /*
      * Setup the Preference Screen.
      * Load the scraper based on the media type sent through the Intent's extras.
@@ -51,113 +37,19 @@ public class ScraperPreferences extends PreferenceActivity {
         super.onCreate(savedInstanceState);
 
         int type = getIntent().getIntExtra("media", 0);
-        mSettings = BaseScraper2.getSettings(type, this);
-        mScraper = BaseScraper2.getScraper(type, this);
-        generatePreferenceScreen();
-    }
 
-    private void generatePreferenceScreen() {
-        // We use a different preference file for each scraper, so we need to
-        // change it.
-        mPm = getPreferenceManager();
-        mPm.setSharedPreferencesName(mScraper.getName());
+        setContentView(R.layout.scraper_preferences_fragment);
 
-        PreferenceScreen screen = createPreferenceHierarchy();
-        if (screen != null) {
-            setPreferenceScreen(screen);
-        } else {
-            setContentView(R.layout.no_settings);
+        Bundle mBundle = new Bundle();
+        mBundle.putString("type", String.valueOf(type));
+        Fragment mScraperPreferencesFragment = getSupportFragmentManager().findFragmentByTag(ScraperPreferencesFragment.FRAGMENT_TAG);
+        if (mScraperPreferencesFragment == null) {
+            mScraperPreferencesFragment = new ScraperPreferencesFragment();
         }
-    }
+        mScraperPreferencesFragment.setArguments(mBundle);
 
-    private PreferenceScreen createPreferenceHierarchy() {
-
-        if(mPm == null || mScraper == null)
-            return null;
-
-        PreferenceScreen root = mPm.createPreferenceScreen(this);
-        SharedPreferences preferences = mPm.getSharedPreferences();
-        ScraperSettings settings = mSettings;
-
-        if(settings == null)
-            return null;
-
-        // Get the settings available for this scraper.
-        Hashtable<String, ScraperSetting> hashSettings = settings.getSettings();
-
-        if(hashSettings == null || hashSettings.size() == 0)
-            return null;
-
-        /*
-         * We iterate on the settings to generate an adequate object, ie. a
-         * checkbox for a boolean, a text input for text, a list for an enum, etc.
-         */
-        for(Enumeration<String> e = hashSettings.keys(); e.hasMoreElements();) {
-            String key = e.nextElement();
-            ScraperSetting value = hashSettings.get(key);
-            switch(value.getType()) {
-                case ScraperSetting.TYPE_BOOL:
-                    CheckBoxPreference togglePref = new CheckBoxPreference(this);
-                    togglePref.setKey(value.getId());
-                    togglePref.setTitle(value.getLabel());
-                    togglePref.setDefaultValue(preferences.getBoolean(value.getId(),
-                            value.getBooleanDefault()));
-                    root.addPreference(togglePref);
-                    break;
-
-                case ScraperSetting.TYPE_INTEGER:
-                    EditTextPreference intPref = new EditTextPreference(this);
-                    intPref.setKey(value.getId());
-                    intPref.setTitle(value.getLabel());
-                    intPref.setDefaultValue(preferences.getInt(value.getId(),
-                            value.getIntDefault()));
-                    root.addPreference(intPref);
-                    break;
-
-                case ScraperSetting.TYPE_TEXT:
-                    EditTextPreference textPref = new EditTextPreference(this);
-                    textPref.setKey(value.getId());
-                    textPref.setTitle(value.getLabel());
-                    textPref.setDefaultValue(preferences.getString(value.getId(),
-                            value.getStringDefault()));
-                    root.addPreference(textPref);
-                    break;
-
-                case ScraperSetting.TYPE_LABELENUM:
-                    // this is the Language Setting.. the only one that is of type labelenum
-                    List<String> items = value.getValues();
-                    Hashtable<String, String> values = new Hashtable<String, String>();
-                    String userLocale = Locale.getDefault().getLanguage();
-                    String localeDefault = null;
-                    for(String item: items) {
-                        Locale loc = new Locale(item);
-                        if (userLocale.equals(item)) {
-                            localeDefault = item;
-                        }
-                        values.put(loc.getDisplayLanguage(), item);
-                    }
-                    String[] keys = values.keySet().toArray(new String[items.size()]);
-                    Arrays.sort(keys);
-                    String[] display = new String[items.size()];
-                    for(int i = 0; i < items.size(); i++)
-                        display[i] = values.get(keys[i]);
-                    ListPreference enumPref = new ListPreference(this);
-                    enumPref.setKey(value.getId());
-                    enumPref.setTitle(value.getLabel());
-                    enumPref.setEntries(keys);
-                    enumPref.setEntryValues(display);
-                    if (localeDefault != null) {
-                        enumPref.setDefaultValue(preferences.getString(value.getId(),
-                                localeDefault));
-                    } else {
-                        enumPref.setDefaultValue(preferences.getString(value.getId(),
-                                value.getStringDefault()));
-                    }
-                    root.addPreference(enumPref);
-                    break;
-            }
-        }
-
-        return root;
+        FragmentTransaction mFragmentTransaction = getSupportFragmentManager().beginTransaction();
+        mFragmentTransaction.replace(R.id.fragment_container, mScraperPreferencesFragment, ScraperPreferencesFragment.FRAGMENT_TAG);
+        mFragmentTransaction.commit();
     }
 }
