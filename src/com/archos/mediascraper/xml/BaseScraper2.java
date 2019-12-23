ee06da4d192b142eaecd89c11931b578b45ba531
Marc de Courville <marc@courville.org>
2019-12-23 16:25:53 +0100

    Add Norwegian and Vietnamese

--- src/com/archos/mediascraper/xml/BaseScraper2.java
+++ src/com/archos/mediascraper/xml/BaseScraper2.java
@@ -41,7 +41,7 @@ import javax.xml.parsers.SAXParserFactory;
 public abstract class BaseScraper2 {
     private static final String TAG = "BaseScraper2";
 
-    protected final static String LANGUAGES = "da|fi|nl|de|it|es|fr|pl|hu|el|tr|ru|he|ja|pt|zh|cs|sl|hr|ko|en|sv|no|pb";
+    protected final static String LANGUAGES = "da|fi|nl|de|it|es|fr|pl|hu|el|tr|ru|he|ja|pt|zh|cs|sl|hr|ko|en|sv|no|pb|no|vi";
 
     protected final SAXParser mParser;
 
