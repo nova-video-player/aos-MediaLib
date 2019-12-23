a50f58be3e66da5426417c56b21c85fe97d85b7e
Marc de Courville <marc@courville.org>
2019-12-23 23:30:42 +0100

    add an example regexp

--- src/com/archos/mediascraper/preprocess/MovieDefaultMatcher.java
+++ src/com/archos/mediascraper/preprocess/MovieDefaultMatcher.java
@@ -144,6 +144,7 @@ class MovieDefaultMatcher implements InputMatcher {
             " anaglyph ", " anaglyphe ", /* Anaglyph 3d stuff */
             " truehd ", " atmos ", " uhd ", " hdr10+ ", " hdr10 ", " hdr ", " dolby ", " dts-x ", " dts-hd.ma ",
     };
+    // denoise filter Default = @"(([\(\{\[]|\b)((576|720|1080)[pi]|dir(ectors )?cut|dvd([r59]|rip|scr(eener)?)|(avc)?hd|wmv|ntsc|pal|mpeg|dsr|r[1-5]|bd[59]|dts|ac3|blu(-)?ray|[hp]dtv|stv|hddvd|xvid|divx|x264|dxva|(?-i)FEST[Ii]VAL|L[iI]M[iI]TED|[WF]S|PROPER|REPACK|RER[Ii]P|REAL|RETA[Ii]L|EXTENDED|REMASTERED|UNRATED|CHRONO|THEATR[Ii]CAL|DC|SE|UNCUT|[Ii]NTERNAL|[DS]UBBED)([\]\)\}]|\b)(-[^\s]+$)?)")]
     // stuff that could be present in real names is matched with tight case sensitive syntax
     // strings here will only match if separated by any of " .-_"
     private static final String[] GARBAGE_CASESENSITIVE = {
