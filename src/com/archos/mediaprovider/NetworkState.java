f4fc72663766a83c70e55109121822b3208604f4
Marc de Courville <marc@courville.org>
2020-03-06 21:46:46 +0100

    Rework the code not to rely on NetworkInfo

--- src/com/archos/mediaprovider/NetworkState.java
+++ src/com/archos/mediaprovider/NetworkState.java
@@ -16,11 +16,10 @@ package com.archos.mediaprovider;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import androidx.preference.PreferenceManager;
 import android.util.Log;
 
+import com.archos.environment.ArchosUtils;
 
 import java.util.WeakHashMap;
 
@@ -28,6 +27,13 @@ import java.util.WeakHashMap;
 public class NetworkState {
     private static final String TAG = ArchosMediaCommon.TAG_PREFIX + NetworkState.class.getSimpleName();
     private static final boolean DBG = false;
+
+    private boolean mConnected;
+    private boolean mConnectedOrConnecting;
+    private boolean mHasLocalConnection;
+    // abusing WeakHashMap to have a list of WeakReferences to Observers
+    private final WeakHashMap<Observer, Void> mObservers = new WeakHashMap<Observer, Void>();
+    private Context mContext;
 
     /** implement & register for observing hasLocalConnection() */
     public interface Observer {
@@ -55,70 +61,32 @@ public class NetworkState {
         return sInstance;
     }
 
-    private int mNetworkType = -1;
-    private boolean mConnected;
-    private boolean mConnectedOrConnecting;
-    private boolean mAvailable;
-    private boolean mHasLocalConnection;
-    // abusing WeakHashMap to have a list of WeakReferences to Observers
-    private final WeakHashMap<Observer, Void> mObservers = new WeakHashMap<Observer, Void>();
-    private Context mContext;
-
-    // ------------------------------ PUBLIC API ---------------------------- //
-    public boolean hasLocalConnection() {
-        return mHasLocalConnection;
+    protected NetworkState(Context context) {
+        mContext = context;
+        updateFrom(context);
     }
 
-    public boolean isConnected() {
-        return mConnected;
-    }
+    public boolean hasLocalConnection() { return mHasLocalConnection; }
 
-    public boolean isConnectedOrConnecting() {
-        return mConnectedOrConnecting;
-    }
-
-    public boolean isAvailable() {
-        return mAvailable;
-    }
-
-    public void addObserver(Observer observer) {
-        if (observer == null) {
-            throw new NullPointerException();
-        }
-        synchronized (this) {
-            if (!mObservers.containsKey(observer))
-                mObservers.put(observer, null);
-        }
-    }
-
-    public synchronized void removeObserver(Observer observer) {
-        mObservers.remove(observer);
-    }
+    public boolean isConnected() { return mConnected; }
 
     public boolean updateFrom(Context context) {
-        ConnectivityManager conn = (ConnectivityManager)
-                context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = conn.getActiveNetworkInfo();
-        return updateFrom(networkInfo);
-    }
-
-    /** update with new NetworkInfo, returns true when that changes hasLocalConnection */
-    public boolean updateFrom(NetworkInfo info) {
-        if (DBG) Log.d(TAG, "NetState update:" + info);
-        if (info != null) {
-            setNetworkType(info.getType());
-            setAvailable(info.isAvailable());
-            setConnected(info.isConnected());
-            setConnectedOrConnecting(info.isConnectedOrConnecting());
-        } else {
-            reset();
+        // returns true when that changes hasLocalConnection
+        boolean returnBoolean = false;
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean connected = ArchosUtils.isNetworkConnected(context);
+        if (connected != mConnected) {
+            if (DBG) Log.d(TAG, "updateFrom: connected changed " + mConnected + "->" +  connected);
+            mConnected = connected;
         }
-
-        if (updateLocalConnection()) {
+        boolean hasLocalConnection = ArchosUtils.isLocalNetworkConnected(mContext) || preferences.getBoolean("vpn_mobile", false);
+        if (hasLocalConnection != mHasLocalConnection) {
+            if (DBG) Log.d(TAG, "updateFrom: connected changed " + mHasLocalConnection + "->" +  hasLocalConnection);
+            mHasLocalConnection = hasLocalConnection;
+            returnBoolean = true;
             handleChange();
-            return true;
         }
-        return false;
+        return returnBoolean;
     }
 
     private synchronized void handleChange() {
@@ -126,74 +94,6 @@ public class NetworkState {
             if (observer != null)
                 observer.onLocalNetworkState(mHasLocalConnection);
         }
-    }
-
-    private boolean updateLocalConnection() {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(mContext);
-        boolean localConnection = mConnected &&
-                (mNetworkType == ConnectivityManager.TYPE_ETHERNET
-                || mNetworkType == ConnectivityManager.TYPE_WIFI
-                || preferences.getBoolean("vpn_mobile", false));
-
-        if (mHasLocalConnection != localConnection) {
-            if (DBG) Log.d(TAG, "localConnection changed " + mHasLocalConnection + " -> " + localConnection);
-            mHasLocalConnection = localConnection;
-            return true;
-        }
-        return false;
-    }
-
-    private boolean setConnected(boolean value) {
-        if (mConnected != value) {
-            if (DBG) Log.d(TAG, "connected changed " + mConnected + " -> " + value);
-            mConnected = value;
-            return true;
-        }
-        return false;
-    }
-
-    private boolean setConnectedOrConnecting(boolean value) {
-        if (mConnectedOrConnecting != value) {
-            if (DBG) Log.d(TAG, "connectedOrConnecting changed " + mConnectedOrConnecting + " -> " + value);
-            mConnectedOrConnecting = value;
-            return true;
-        }
-        return false;
-    }
-
-    private boolean setAvailable(boolean value) {
-        if (mAvailable != value) {
-            if (DBG) Log.d(TAG, "available changed " + mConnectedOrConnecting + " -> " + value);
-            mAvailable = value;
-            return true;
-        }
-        return false;
-    }
-
-    private boolean setNetworkType(int type) {
-        if (mNetworkType != type) {
-            if (DBG) Log.d(TAG, "networkType changed " + mNetworkType + " -> " + type);
-            mNetworkType = type;
-            return true;
-        }
-        return false;
-    }
-
-    private boolean reset() {
-        boolean change = false;
-        change |= setNetworkType(-1);
-        change |= setAvailable(false);
-        change |= setConnected(false);
-        change |= setConnectedOrConnecting(false);
-        return change;
-    }
-
-    protected NetworkState(Context context) {
-        mContext = context;
-        ConnectivityManager conn = (ConnectivityManager)
-                context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = conn.getActiveNetworkInfo();
-        updateFrom(networkInfo);
     }
 
 }
