91a842830febf4ce3806e32359f70981858c20a1
bronnel <austin.316scsa@yahoo.com>
2022-02-27 17:02:28 +0300

    Minor fix in ShowIdParser

--- src/com/archos/mediascraper/themoviedb3/ShowIdParser.java
+++ src/com/archos/mediascraper/themoviedb3/ShowIdParser.java
@@ -84,8 +84,8 @@ public class ShowIdParser {
 
         if (serie.networks != null) {
                 for (int i = 0; i < serie.networks.size(); i++) {
-                    result.addNetworkLogoGITHUB(mContext, serie.networks.get(i).name.replaceAll(" ", "%20") + ".png");
                     log.debug("getResult: " + serie.id + " has networklogo_path=" + ScraperImage.GNL + serie.networks.get(i).name.replaceAll(" ", "%20") + ".png");
+                    result.addNetworkLogoGITHUB(mContext, serie.networks.get(i).name.replaceAll(" ", "%20") + ".png");
                 }
         } else log.debug("getResult: no networklogo_path for " + serie.id);
 
