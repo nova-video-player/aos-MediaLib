063d1ee0b887674f01532fd14dace5383634c178
Courville Software <software@courville.org>
2023-09-03 14:19:12 +0200

    Add uri check methods
    See https://github.com/nova-video-player/aos-AVP/issues/943

--- src/com/archos/mediacenter/filecoreextension/UriUtils.java
+++ src/com/archos/mediacenter/filecoreextension/UriUtils.java
@@ -18,13 +18,26 @@ import android.net.Uri;
 
 import com.archos.filecorelibrary.FileUtils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by alexandre on 25/06/15.
  */
 public class UriUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(UriUtils.class);
 
     public final static List<String> networkSharesTypes = List.of("ftp", "sftp", "ftps", "sshj", "smb", "smbj", "webdav", "webdavs", "upnp");
     private final static int maxUriType = networkSharesTypes.size();
@@ -48,6 +61,67 @@ public class UriUtils {
 
     public static boolean isValidUriType(int type) {
         return (type > -1 && type < getNumberUriTypes());
+    }
+
+    public static boolean isFtpBasedProtocol(int type) {
+        if (type > maxUriType || type < 0) return false;
+        return networkSharesTypes.get(type).contains("ftp");
+    }
+
+    private static final String HOSTNAME_PATTERN =
+            "^(?=.{1,255}$)[0-9A-Za-z](?:(?:[0-9A-Za-z]|-){0,61}[0-9A-Za-z])?(?:\\.[0-9A-Za-z](?:(?:[0-9A-Za-z]|-){0,61}[0-9A-Za-z])?)*$";
+
+    public static boolean isValidHost(String hostname) {
+        if (hostname == null) return false;
+        Pattern pattern = Pattern.compile(HOSTNAME_PATTERN);
+        Matcher matcher = pattern.matcher(hostname);
+        return matcher.matches();
+    }
+
+    public static boolean isValidPath(String pathname) {
+        if (pathname == null) return false;
+        File file = new File(pathname);
+        try {
+            String canonicalPath = file.getCanonicalPath();
+            log.debug("isValidPath: input=" + pathname + " -> canonicalPath=" + canonicalPath);
+            return canonicalPath.equals(pathname);
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public static boolean isValidPort(String input) {
+        try {
+            int port = Integer.parseInt(input);
+            // Check if the port is within the valid range (0-65535)
+            return port >= 0 && port <= 65535;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static boolean isValidPort(int port) {
+        return port >= -1 && port <= 65535;
+    }
+
+    public static boolean allowsEmptyCredential(String scheme) {
+        return "webdav".equals(scheme)||"webdavs".equals(scheme)||
+                "smb".equals(scheme)||"smbj".equals(scheme);
+    }
+
+    // sftp requires username
+    public static boolean emptyCredentialMeansAnonymous(String scheme) {
+        return "webdav".equals(scheme)||"webdavs".equals(scheme)||"ftp".equals(scheme)||"ftps".equals(scheme);
+    }
+
+    public static boolean requiresDomain(String scheme) {
+        if (scheme == null) return false;
+        return scheme.startsWith("smb");
+    }
+
+    public static boolean requiresDomain(int position) {
+        if (position > maxUriType || position < 0) return false;
+        return networkSharesTypes.get(position).startsWith("smb");
     }
 
     public static boolean isIndexable(Uri uri){
@@ -86,10 +160,6 @@ public class UriUtils {
     public static Integer getUriType(Uri uri){
         // -1 if not found
         return networkSharesTypes.indexOf(uri.getScheme());
-    }
-
-    public static boolean doesUriTypeRequiresDomain(int type){
-        return (networkSharesTypes.get(type) == "smb" || networkSharesTypes.get(type) == "smbj");
     }
 
     public static String getTypeUri(Integer type) throws IllegalArgumentException {
