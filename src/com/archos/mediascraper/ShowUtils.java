f7629e290d7eb0cf3fed8ac9cd3512a9b55a2c8b
Courville Software <software@courville.org>
2026-06-16 21:03:42 +0200

    scraper: keep numeric movie titles from matching SSEE
    See https://github.com/nova-video-player/aos-AVP/issues/1775

--- src/com/archos/mediascraper/ShowUtils.java
+++ src/com/archos/mediascraper/ShowUtils.java
@@ -88,7 +88,8 @@ public final class ShowUtils {
             //Pattern.compile("(.+)" + SEP_MANDATORY + "(?!(?:264|265|720))([1-9])(\\d{2,2})" + SEP_MANDATORY + ".*", Pattern.CASE_INSENSITIVE),
             // 4-digit SSEE format e.g. "Battlestar Galactica - 0208 - Final cut" -> Season 2, Episode 8
             // Excludes year-prefixed numbers (19xx, 20xx) and resolution numbers preceded by 'x' (1920x1080)
-            Pattern.compile("(.+?)" + SEP_MANDATORY + "(?!(?:19|20)\\d{2})(?<![xX])([0-9]{2})([0-9]{2})(?!\\d)" + SEP_MANDATORY + ".*", Pattern.CASE_INSENSITIVE),
+            // Also reject compact numbers followed by an explicit release year, e.g. "Chambre 1408 (2007)" is a movie, not S14E08.
+            Pattern.compile("(.+?)" + SEP_MANDATORY + "(?!(?:19|20)\\d{2})(?<![xX])([0-9]{2})([0-9]{2})(?!\\d)(?!" + SEP_MANDATORY + "(?:19|20)\\d{2}(?:$|" + SEP_MANDATORY + "))" + SEP_MANDATORY + ".*", Pattern.CASE_INSENSITIVE),
             // Daily shows The Talk 2023 05 05 XviD-AFG [eztv].mkv -> s2023e0505 ou s2023e(m*31+d)
             Pattern.compile("(.+?)" + SEP_MANDATORY + "(20\\d{2}|19\\d{2}|\\d{1,2})" + SEP_MANDATORY + "((\\d{2})" + SEP_MANDATORY + "(\\d{2})(?!\\d)).*", Pattern.CASE_INSENSITIVE),
             // Match show EXX -> one season?
